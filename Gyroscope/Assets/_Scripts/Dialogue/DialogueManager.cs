bc0c8d325f559f29cc54aa53dd63315325593450
Jeppe Faber <JeppestougaardFaber@gmail.com>
2020-04-27 19:50:06 +0200

    Working on UI dialogue system - init'ed Text Mesh Pro

--- Gyroscope/Assets/_Scripts/Dialogue/DialogueManager.cs
+++ Gyroscope/Assets/_Scripts/Dialogue/DialogueManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -23,7 +24,11 @@ public class DialogueManager : MonoBehaviour
     }
     #endregion
 
-    private Queue<string> sentences;
+    public Canvas DialogueUIParent;
+    public Text speakerText;
+    public Text SentenceText;
+
+    private Queue<Dialogue.SentenceCombo> sentences;
 
     private bool dialogueInProgress;
 
@@ -35,16 +40,16 @@ public class DialogueManager : MonoBehaviour
 
     private void Start()
     {
-        sentences = new Queue<string>();
+        sentences = new Queue<Dialogue.SentenceCombo>();
     }
 
     private void Update()
     {
         if (dialogueInProgress)
         {
-            if (Input.GetAxis("Submit") == 1)
+            if (Input.GetButtonDown("Submit"))
             {
-                Debug.Log("next sentence");
+                DisplayNextSentence();
             }
         }
     }
@@ -58,7 +63,7 @@ public class DialogueManager : MonoBehaviour
 
         foreach (Dialogue.SentenceCombo sentenceCombo in dialogue.sentences)
         {
-            this.sentences.Enqueue(sentenceCombo.sentence);
+            this.sentences.Enqueue(sentenceCombo);
         }
 
         DisplayNextSentence();
@@ -72,8 +77,11 @@ public class DialogueManager : MonoBehaviour
             return;
         }
 
-        string sentence = sentences.Dequeue();
-        Debug.Log("First sentence is: " + sentence);
+        Dialogue.SentenceCombo sentenceCombo = sentences.Dequeue();
+        string speaker = sentenceCombo.speakerName;
+        string sentence = sentenceCombo.sentence;
+
+        Debug.Log($"{speaker} says: {sentence}");
     }
 
     private void EndDialogue()
