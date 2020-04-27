b10eaa5afb568e8db24dbbc377f53b38b60845d6
Jeppe Faber <JeppestougaardFaber@gmail.com>
2020-04-28 00:06:22 +0200

    Working on placing invisible walls in forest area

---
+++ Gyroscope/Assets/_Scripts/AlwaysShowColliderGizmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysShowColliderGizmo : MonoBehaviour
+{
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Matrix4x4 prevMatrix = Gizmos.matrix;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+        Gizmos.matrix = prevMatrix;
+    }
+}
