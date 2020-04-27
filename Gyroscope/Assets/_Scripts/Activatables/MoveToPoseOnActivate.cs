d8ad4c3e8f2443d76318938410556c1a40ac021d
Per Kristian Sørensen <peks@itu.dk>
2020-04-27 18:52:02 +0200

    TunnelUpdate
    Tunnel fits, but is more boring

--- Gyroscope/Assets/_Scripts/Activatables/MoveToPoseOnActivate.cs
+++ Gyroscope/Assets/_Scripts/Activatables/MoveToPoseOnActivate.cs
@@ -6,11 +6,17 @@ using UnityEngine;
 public class MoveToPoseOnActivate : ActivateableBase
 {
     [SerializeField]private AnimationCurve Easing;
+    [SerializeField] private Transform Target;
+
     [SerializeField]private Vector3 LocalOffsetOnInactive;
     [SerializeField] private Transform ActivatorTrans;
     private Vector3 StartPos;
     private Vector3 TargetPos;
+    [SerializeField]private float MaxSpeed;
     [SerializeField]private Vector2 MinMaxDistanceEffect;
+
+    private Vector3 GetTargetPos => Target != null ? Target.position : StartPos;
+    private Vector3 GetTargetPosGizmos => Target != null ? Target.position : transform.position;
     public override void Activate(GameObject activator)
     {
         ActivatorTrans = activator.transform;
@@ -32,11 +38,28 @@ public class MoveToPoseOnActivate : ActivateableBase
         float t = 1;
         if (ActivatorTrans != null)
         { 
-            t = StaticMath.NormalizeValue(Vector3.Distance(ActivatorTrans.position, StartPos), MinMaxDistanceEffect.x,
+            t = StaticMath.NormalizeValue(Vector3.Distance(ActivatorTrans.position, GetTargetPos), MinMaxDistanceEffect.x,
                 MinMaxDistanceEffect.y);
         }
 
         t = Easing.Evaluate(Mathf.Clamp01(t));
         transform.position = Vector3.Lerp(StartPos, TargetPos , t);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (GetComponent<Collider>() == null)
+        {
+            return;
+        }
+        
+        Gizmos.DrawWireCube(transform.position + transform.TransformVector(LocalOffsetOnInactive), GetComponent<Collider>().bounds.size);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(GetTargetPosGizmos, MinMaxDistanceEffect.y);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(GetTargetPosGizmos, MinMaxDistanceEffect.x);
+        Gizmos.DrawSphere(GetTargetPosGizmos, 1);
+
+        Gizmos.DrawLine(GetTargetPosGizmos, transform.position + transform.TransformVector(LocalOffsetOnInactive));
     }
 }
