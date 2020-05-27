5548ee7ff0b2eec31bc92c297731ec6ce11a1859
Jeppe Faber <JeppestougaardFaber@gmail.com>
2020-05-27 10:14:58 +0200

    Cave section now turns off aura and requires player to turn it back on

---
+++ Gyroscope/Assets/_Scripts/PlayerInteraction/PowerReFillScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerReFillScript : OnClickInteractable
+{
+
+    public ProxyActivateColliderTrigger thething;
+    public override void HandleInteract(GameObject sender)
+    {
+        base.HandleInteract(sender);
+
+        thething.ActivateEverything();
+
+        Destroy(this.gameObject);
+    }
+
+
+}
--- Gyroscope/Assets/_Scripts/SimpleTriggers/ProxyActivateColliderTrigger.cs
+++ Gyroscope/Assets/_Scripts/SimpleTriggers/ProxyActivateColliderTrigger.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 public class ProxyActivateColliderTrigger : MonoBehaviour
 {
     public Collider colliderTarget;
+    public Collider colliderTarget2;
     public Renderer RendererToChange;
     public Renderer glowRenderer;
 
+    public float toneTime;
+
     public bool triggered;
+
+    [Header("Original Values - resets to this after everything is done")]
+    public float originalMinDistanceValue = 30;
+    public float originalMaxDistanceValue = 50;
+    public float originalEmmisionStrengthValue = 50;
 
     Dictionary<string, matPair> storedVals;
 
@@ -36,35 +44,111 @@ public class ProxyActivateColliderTrigger : MonoBehaviour
         storedVals.Add("emmisionStrength", new matPair("Vector1_C05E6C68", glowRenderer.sharedMaterial.GetFloat("Vector1_C05E6C68"))); //emmision brightness on 
 
         //nothing worked so were just doing this manually
-        RendererToChange.sharedMaterial.SetFloat(storedVals["minDistance"].field, 30);
-        RendererToChange.sharedMaterial.SetFloat(storedVals["maxDistance"].field, 50);
+        RendererToChange.sharedMaterial.SetFloat(storedVals["minDistance"].field, originalMinDistanceValue);
+        RendererToChange.sharedMaterial.SetFloat(storedVals["maxDistance"].field, originalMaxDistanceValue);
 
-        glowRenderer.sharedMaterial.SetFloat(storedVals["emmisionStrength"].field, 50);
+        glowRenderer.sharedMaterial.SetFloat("Vector1_C05E6C68", originalEmmisionStrengthValue);
     }
 
     private void OnTriggerEnter(Collider collision)
     {
+        DeactivateEverything(collision);
+    }
+
+    private void DeactivateEverything(Collider collision)
+    {
         if (!triggered)
         {
             if (collision.CompareTag(StaticVariables.Tags.Player))
-                colliderTarget.enabled = true;
-            Debug.Log("Triggered!");
-            RendererToChange.sharedMaterial.SetFloat("Vector1_1DEA01FF", 0.1f);
-            RendererToChange.sharedMaterial.SetFloat("Vector1_4F9399E", 0.2f);
+            {
+                //colliderTarget.enabled = true;
+                //RendererToChange.sharedMaterial.SetFloat("Vector1_1DEA01FF", 0.1f);
+                //RendererToChange.sharedMaterial.SetFloat("Vector1_4F9399E", 0.2f);
 
-            glowRenderer.sharedMaterial.SetFloat("Vector1_C05E6C68", 0f);
+                //glowRenderer.sharedMaterial.SetFloat("Vector1_C05E6C68", 0f);
 
-            triggered = true;
+                StartCoroutine(toneDownLights(toneTime));
+
+                triggered = true;
+            }
         }
+    }
+
+    public void ActivateEverything()
+    {
+
+        //disable colliders
+        colliderTarget.enabled = false;
+        colliderTarget2.enabled = false;
+
+        ////nothing worked so we're just resetting values manually, gotta change  this if we change fields
+        //RendererToChange.sharedMaterial.SetFloat(storedVals["minDistance"].field, originalMinDistanceValue);
+        //RendererToChange.sharedMaterial.SetFloat(storedVals["maxDistance"].field, originalMaxDistanceValue);
+
+        //glowRenderer.sharedMaterial.SetFloat("Vector1_C05E6C68", originalEmmisionStrengthValue);
+
+        StartCoroutine(toneUpLights(toneTime));
     }
 
     private void OnDestroy()
     {
-        //reset all shared changes to shared materials based on values recorded at start...
-        // we do this because sharedMaterial changes asset itself, so changes carry over to non-playmode 
-        RendererToChange.sharedMaterial.SetFloat(storedVals["minDistance"].field, 30);
-        RendererToChange.sharedMaterial.SetFloat(storedVals["maxDistance"].field, 50);
+        //nothing worked so we're just doin this manually
+        RendererToChange.sharedMaterial.SetFloat(storedVals["minDistance"].field, originalMinDistanceValue);
+        RendererToChange.sharedMaterial.SetFloat(storedVals["maxDistance"].field, originalMaxDistanceValue);
 
-        glowRenderer.sharedMaterial.SetFloat(storedVals["emmisionStrength"].field, 50);
+        glowRenderer.sharedMaterial.SetFloat("Vector1_C05E6C68", originalEmmisionStrengthValue);
+    }
+
+    private IEnumerator toneDownLights(float toneDownTime)
+    {
+        yield return null;
+        float timer = 0;
+
+        while (timer < toneDownTime)
+        {
+            Debug.Log("toning down");
+            float normalizedTime = timer / toneDownTime;
+            //nothing worked so we're just resetting values manually, gotta change  this if we change fields
+            RendererToChange.sharedMaterial.SetFloat(storedVals["minDistance"].field, originalMinDistanceValue * (1-normalizedTime));
+            RendererToChange.sharedMaterial.SetFloat(storedVals["maxDistance"].field, originalMaxDistanceValue * (1-normalizedTime));
+
+            glowRenderer.sharedMaterial.SetFloat("Vector1_C05E6C68", originalEmmisionStrengthValue * (1-normalizedTime));
+
+            timer += Time.deltaTime;
+
+            yield return null;
+        }
+        //nothing worked so we're just resetting values manually, gotta change  this if we change fields
+        RendererToChange.sharedMaterial.SetFloat(storedVals["minDistance"].field, 0.1f);
+        RendererToChange.sharedMaterial.SetFloat(storedVals["maxDistance"].field, 0.2f);
+
+        glowRenderer.sharedMaterial.SetFloat("Vector1_C05E6C68", 0);
+
+    }
+
+    private IEnumerator toneUpLights(float toneUpTime)
+    {
+        yield return null;
+        float timer = 0;
+
+        while (timer < toneUpTime)
+        {
+
+            float normalizedTime = timer / toneUpTime;
+            //nothing worked so we're just resetting values manually, gotta change  this if we change fields
+            RendererToChange.sharedMaterial.SetFloat(storedVals["minDistance"].field, originalMinDistanceValue * normalizedTime);
+            RendererToChange.sharedMaterial.SetFloat(storedVals["maxDistance"].field, originalMaxDistanceValue * normalizedTime);
+
+            glowRenderer.sharedMaterial.SetFloat("Vector1_C05E6C68", originalEmmisionStrengthValue * normalizedTime);
+
+            timer += Time.deltaTime;
+
+            yield return null;
+        }
+        //nothing worked so we're just resetting values manually, gotta change  this if we change fields
+        RendererToChange.sharedMaterial.SetFloat(storedVals["minDistance"].field, originalMinDistanceValue);
+        RendererToChange.sharedMaterial.SetFloat(storedVals["maxDistance"].field, originalMaxDistanceValue);
+
+        glowRenderer.sharedMaterial.SetFloat("Vector1_C05E6C68", originalEmmisionStrengthValue);
     }
 }
